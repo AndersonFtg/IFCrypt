c5e05073d65f3cf5ba5a72dec9b9b431085a1d65
Anderson Fostinger <48109661+AndersonFtg@users.noreply.github.com>
2021-06-09 00:18:45 -0300

    Add files via upload

---
+++ src/controllers/ControllerDescriptografar.java
@@ -0,0 +1,148 @@
+package controllers;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ResourceBundle;
+
+import javax.crypto.NoSuchPaddingException;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import models.arquivo.SelecaoArquivo;
+import models.criptografia.DescriptografarArquivo;
+import models.util.MensagemAoUsuario;
+
+public class ControllerDescriptografar implements Initializable {
+
+	@FXML
+	private TextField txtArquivoDecript, txtChavePubDecript, txtChavePrivDecript;
+	
+	@FXML
+	private Button btnDecriptDoc;
+
+	private File docCriptografado, localSalvarDocDescriptografado, docChavePubRemetente, docChavePrivDestino;
+	private SelecaoArquivo arquivo;
+	private DescriptografarArquivo descriptArquivo;
+	private MensagemAoUsuario mensagem;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		arquivo = new SelecaoArquivo();
+		mensagem = new MensagemAoUsuario();
+		
+		Platform.runLater(() -> txtArquivoDecript.requestFocus());
+	}
+
+	@FXML
+	protected void escolherArquivo() {
+		docCriptografado = arquivo.selecionarArquivoCifrado();
+		txtArquivoDecript.setText((docCriptografado == null) ? "" : docCriptografado.getAbsolutePath());
+	}
+	
+	@FXML
+	protected void escolherChavePublica() {
+		docChavePubRemetente = arquivo.selecionarChavePub();
+		txtChavePubDecript.setText((docChavePubRemetente == null) ? "" : docChavePubRemetente.getAbsolutePath());
+	}
+
+	@FXML
+	protected void escolherChavePrivada() {
+		docChavePrivDestino = arquivo.selecionarChavePriv();
+		txtChavePrivDecript.setText((docChavePrivDestino == null) ? "" : docChavePrivDestino.getAbsolutePath());
+	}
+
+	@FXML
+	protected void descriptografarArquivo() {
+		
+		btnDecriptDoc.setDisable(true);
+		mensagem.mensagemProcessamento();
+		
+		try {
+			descriptArquivo = new DescriptografarArquivo();
+			
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException ex) {
+			ex.printStackTrace();
+		}
+		
+		if (verificaSeTxtsEstaoVazios()) {
+			mensagem.mensagemErro();
+			btnDecriptDoc.setDisable(false);
+		}
+		
+		else {
+			
+			boolean assinaturaValida = false;
+		
+			try {
+				assinaturaValida = descriptArquivo.verificarAssinatura(docCriptografado, docChavePubRemetente);
+			
+			} catch (InvalidKeyException | InvalidKeySpecException | NoSuchAlgorithmException | SignatureException
+					| IOException ex) {
+				mensagem.mensagemErroValidarAssinatura();
+				btnDecriptDoc.setDisable(false);
+				ex.printStackTrace();
+			}
+			
+			if(assinaturaValida) {
+				localSalvarDocDescriptografado = arquivo.selecionarLocalSalvarArquivoDecifrado();
+				
+				if (localSalvarDocDescriptografado == null) {
+					mensagem.mensagemAlerta();
+					btnDecriptDoc.setDisable(false);
+				}
+		
+				else {
+					try {	
+						if (descriptArquivo.descriptografar(docCriptografado, localSalvarDocDescriptografado, docChavePrivDestino)) {
+							mensagem.mensagemSucesso("descriptografado");
+							limpaTxts();
+							btnDecriptDoc.setDisable(false);
+						}
+		
+						else {
+							mensagem.mensagemErroFinal("descriptografar");
+							btnDecriptDoc.setDisable(false);
+						}
+		
+					} catch (NoSuchAlgorithmException | InvalidKeyException | InvalidKeySpecException
+							| IOException | SignatureException e) {
+						mensagem.mensagemErroFinal("descriptografar");
+						btnDecriptDoc.setDisable(false);
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			else {
+				mensagem.mensagemAssinaturaInvalida();
+				btnDecriptDoc.setDisable(false);
+			}
+		}
+	}
+
+	private boolean verificaSeTxtsEstaoVazios() {
+
+		if (txtArquivoDecript.getText().equals("") || txtChavePubDecript.getText().equals("")
+				|| txtChavePrivDecript.getText().equals("")) {
+			return true;
+		}
+
+		return false;
+	}
+	
+	private void limpaTxts() {
+		
+		txtArquivoDecript.clear();
+		txtChavePubDecript.clear();
+		txtChavePrivDecript.clear();
+		txtArquivoDecript.requestFocus();
+	}
+}
---
+++ src/models/util/MensagemAoUsuario.java
@@ -0,0 +1,100 @@
+package models.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.ImageView;
+
+public class MensagemAoUsuario {
+
+	private Alert alert;
+
+	public void mensagemErro() {
+
+		alert = new Alert(AlertType.ERROR);
+		alert.setGraphic(new ImageView(this.getClass().getResource("../../images/error.png").toString()));
+		alert.setTitle("Erro");
+		alert.setHeaderText("Campos Vazios");
+		alert.setContentText("Preencha todos os campos para continuar.");
+		alert.showAndWait();
+	}
+
+	public void mensagemErroFinal(String texto) {
+		
+		alert = new Alert(AlertType.ERROR);
+		alert.setGraphic(new ImageView(this.getClass().getResource("../../images/cript_falha.png").toString()));
+		alert.setTitle("Erro");
+		alert.setHeaderText("Falha");
+		alert.setContentText("Ocorreu um errro ao tentar " + texto + " o arquivo. Verifique os arquivos escolhidos.");
+		alert.showAndWait();
+	}
+
+	public void mensagemSucesso(String texto) {
+		
+		alert = new Alert(AlertType.INFORMATION);
+		alert.setGraphic(new ImageView(this.getClass().getResource("../../images/cript_sucesso.png").toString()));
+		alert.setTitle("Sucesso");
+		alert.setHeaderText("Sucesso");
+		alert.setContentText("Arquivo " + texto + " com sucesso!");
+		alert.showAndWait();
+	}
+
+	public void mensagemAlerta() {
+
+		alert = new Alert(AlertType.WARNING);
+		alert.setGraphic(new ImageView(this.getClass().getResource("../../images/warning.png").toString()));
+		alert.setTitle("Atenção");
+		alert.setHeaderText("Nenhum Arquivo Selecionado");
+		alert.setContentText("Por favor, selecione um arquivo para continuar.");
+		alert.showAndWait();
+	}
+	
+	public void mensagemAssinaturaInvalida() {
+		
+		alert = new Alert(AlertType.ERROR);
+		alert.setGraphic(new ImageView(this.getClass().getResource("../../images/error.png").toString()));
+		alert.setTitle("Erro");
+		alert.setHeaderText("Assinatura");
+		alert.setContentText("Assinatura Inválida. Podem ter ocorrido modificações no documento ou pode ter sido corrompido.");
+		alert.showAndWait();
+	}
+	
+	public void mensagemErroValidarAssinatura() {
+		
+		alert = new Alert(AlertType.ERROR);
+		alert.setGraphic(new ImageView(this.getClass().getResource("../../images/error.png").toString()));
+		alert.setTitle("Erro");
+		alert.setHeaderText("Assinatura");
+		alert.setContentText("Ocorreu um erro ao validar a assinatura do arquivo. Favor verificar os arquivos escolhidos.");
+		alert.showAndWait();
+	}
+	
+	public void mensagemChaveSucesso() {
+		
+		alert = new Alert(AlertType.INFORMATION);
+		alert.setGraphic(new ImageView(this.getClass().getResource("../../images/success.png").toString()));
+		alert.setTitle("Sucesso");
+		alert.setHeaderText("Chaves RSA");
+		alert.setContentText("Chaves RSA Geradas com Sucesso.");
+		alert.showAndWait();
+	}
+	
+	public void mensagemChaveErro() {
+		
+		alert = new Alert(AlertType.ERROR);
+		alert.setGraphic(new ImageView(this.getClass().getResource("../../images/error.png").toString()));
+		alert.setTitle("Erro");
+		alert.setHeaderText("Chaves RSA");
+		alert.setContentText("Ocorreu um erro ao salvar as chaves. Verifique os campos preenchidos e tente novamente.");
+		alert.showAndWait();
+	}
+
+	public void mensagemProcessamento() {
+		
+		alert = new Alert(AlertType.WARNING);
+		alert.setGraphic(new ImageView(this.getClass().getResource("../../images/warning.png").toString()));
+		alert.setTitle("Atenção");
+		alert.setHeaderText("Procesamento");
+		alert.setContentText("O mesmo arquivo não poderá ser descriptografado novamente.");
+		alert.showAndWait();
+	}
+}
